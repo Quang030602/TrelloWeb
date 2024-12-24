55706b71309c49d74cc6f35fa9a4d0e2713abbfb
Quang030602 <minhquang030602@gmail.com>
2024-12-24 13:59:45 +0700

    add CssBaseLine and remove .css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import './App.css'
 import Button from '@mui/material/Button';
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation';
