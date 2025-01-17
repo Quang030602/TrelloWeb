2a841da47fbfc004419c7304db27df075e222ab1
Quang030602 <minhquang030602@gmail.com>
2025-01-17 14:49:22 +0700

    use axios to get data from BE

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -1,18 +1,27 @@
 import {
   Container,
 } from '@mui/material';
-import React from 'react';
+import { useState , useEffect } from 'react';
 import AppBar from '~/components/AppBar/AppBar';
 import BoardBar from './BoardBar/BoardBar';
 import BoardContent from './BoardContent/BoardContent';
 import {mockData} from '~/apis/mock_data';
+import { fetchBoardDetailsAPI } from '~/apis';
 
 function Board(){
+  const [board, setBoard] =  useState(null);
+  useEffect(() => {
+      const boardId = '6789fdc9cf0268ffedcecb82';
+
+      fetchBoardDetailsAPI(boardId).then(board => {
+        setBoard(board)
+      })
+  },[])
     return (  
       <Container  disableGutters  maxWidth={false}  sx={{ height: '100vh'}}>
         <AppBar />  
-        <BoardBar board={mockData?.board} />
-        <BoardContent board={mockData?.board} />
+        <BoardBar board={board} />
+        <BoardContent board={board} />
           
       </Container>
       
