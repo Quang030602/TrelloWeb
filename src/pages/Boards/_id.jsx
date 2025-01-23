62b3fa9c397f8539e843a338e53123c2611e6f4c
Quang030602 <minhquang030602@gmail.com>
2025-01-23 10:36:45 +0700

    setting toast-toastify

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -5,23 +5,23 @@ import { useState , useEffect } from 'react';
 import AppBar from '~/components/AppBar/AppBar';
 import BoardBar from './BoardBar/BoardBar';
 import BoardContent from './BoardContent/BoardContent';
-import {mockData} from '~/apis/mock_data';
+//import {mockData} from '~/apis/mock_data';
 import { fetchBoardDetailsAPI } from '~/apis';
 
 function Board(){
   const [board, setBoard] =  useState(null);
-  useEffect(() => {
-      const boardId = '678f0e3eb49dcbaf6eefa73b';
+  // useEffect(() => {
+  //     const boardId = '678f0e3eb49dcbaf6eefa73b';
 
-      fetchBoardDetailsAPI(boardId).then(board => {
-        setBoard(board)
-      })
-  },[])
+  //     fetchBoardDetailsAPI(boardId).then(board => {
+  //       setBoard(board)
+  //     })
+  // },[])
     return (  
       <Container  disableGutters  maxWidth={false}  sx={{ height: '100vh'}}>
         <AppBar />  
-        <BoardBar board={mockData.board} />
-        <BoardContent board={mockData.board} />
+        <BoardBar board={board} />
+        <BoardContent board={board} />
           
       </Container>
       
