d5d9b615ce68103f87f28796c01507711eee9662
Quang030602 <minhquang030602@gmail.com>
2025-02-20 09:50:15 +0700

    add API for update data

--- src/apis/index.js
+++ src/apis/index.js
@@ -15,3 +15,9 @@ export const createNewCardAPI = async (newCardData) => {
     const response = await axios.post(`${API_ROOT}/v1/cards`,newCardData)
     return response.data
 }
+
+
+export const updateBoardDetailsAPI = async (boardId,updateData) => {
+    const response = await axios.get(`${API_ROOT}/v1/boards/${boardId}`,updateData);
+    return response.data;
+}
